174c36024e7f6ba2e409111c91d70b4915c619d8
cloud8little <413074538@qq.com>
2024-12-18 03:29:41 +0000

    fix clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 for i in {1..4}; do	
-  HOMEDIR=/workspaces/go-2/ExocoreValidators/node$i	
+  HOMEDIR=node$i	
   rm -rf $HOMEDIR/data/*.db $HOMEDIR/data/snapshots $HOMEDIR/data/cs.wal
   jq -n '{"height": "0", "round": 0, "step": 0}' > $HOMEDIR/data/priv_validator_state.json
 done  
