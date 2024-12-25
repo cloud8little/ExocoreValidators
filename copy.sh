b4f3b596db8ede4479ad1f78b02cd0f64ecbc06d
cloud8little <413074538@qq.com>
2024-12-25 15:14:40 +0000

    update genesis time in genesis_v7_enableoracle json file

--- copy.sh
+++ copy.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# please note this script will automatically update the genesis_time in the genesis file
+GENESIS=genesis_v7_enableoracle.json
+TMP_GENESIS=genesis_v7_enableoracle_tmp.json
+
+# Get the current UTC time
+current_utc_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+# Print the current UTC time
+echo "Current UTC time: $current_utc_time"
+
+# Update the genesis_time in the JSON file
+jq --arg time "$current_utc_time" '.genesis_time=$time' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+
 for i in {1..4}; do
   cp genesis_v7_enableoracle.json node$i/config/genesis.json
 done
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exocored start --home ./node1 --minimum-gas-prices 0.0001hua --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --grpc.enable true --json-rpc.enable true --json-rpc.address 0.0.0.0:9545 --oracle &
+exocored start --home ./node1 --minimum-gas-prices 0.0001hua --log_format json --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --grpc.enable true --json-rpc.enable true --json-rpc.address 0.0.0.0:9545 --oracle > "out1.log"  2>&1 &
 pid1 = $!
 
 for i in {2..4}; do
