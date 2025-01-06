2c710274f313c29e0cb38b1cc70520e8961fc6df
cloud8little <413074538@qq.com>
2025-01-06 08:09:00 +0000

    rollback script change

--- assetsTool/7_delegateNST.sh
+++ assetsTool/7_delegateNST.sh
@@ -1,5 +1,5 @@
 #valdiator1
-./assetcli delegate --rpcUrl http://localhost:8545 --staker 0xa53f68563D22EB0dAFAA871b6C08a6852f91d627 --amount 32000000000000000000 --privateKey C26A874A75B028638D477DDF31EB8627899CB505798DF70D2DD2A631F9CAE7A4  --defaultAssetID 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee --layerZeroID 40217 --operator exo1hj3qk6wg7se6l8g3s3ept7aas37dc75fk3lm2s
+./assetcli delegate --rpcUrl http://localhost:9545 --staker 0xa53f68563D22EB0dAFAA871b6C08a6852f91d627 --amount 32000000000000000000 --privateKey C26A874A75B028638D477DDF31EB8627899CB505798DF70D2DD2A631F9CAE7A4  --defaultAssetID 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee --layerZeroID 40217 --operator exo1hj3qk6wg7se6l8g3s3ept7aas37dc75fk3lm2s
 #validator2
 ./assetcli delegate --rpcUrl http://localhost:9545 --staker 0x6e5eE3e436539f46455b5174411942F520c1120E --amount 32000000000000000000 --privateKey C26A874A75B028638D477DDF31EB8627899CB505798DF70D2DD2A631F9CAE7A4  --defaultAssetID 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee --layerZeroID 40217 --operator exo1hj3qk6wg7se6l8g3s3ept7aas37dc75fk3lm2s
 #validator3
